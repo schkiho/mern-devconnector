1504efb56663ed6fe8af3e569da5a09428df51e9
schkiho <schkiho@yahoo.de>
2019-06-13 11:41:53 +0200

    add create & update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -84,8 +84,28 @@ router.post(
     if (facebook) profileFields.social.facebook = facebook;
     if (linkedin) profileFields.social.linkedin = linkedin;
     if (instagram) profileFields.social.instagram = instagram;
-    console.log(profileFields.social.youtube);
-    res.send("hello");
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      if (profile) {
+        // UPdate
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+
+      // Create
+      profile = new Profile(profileFields);
+
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
