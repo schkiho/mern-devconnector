09ad660a16f51d452f699ca833219055b37c10b0
schkiho <schkiho@yahoo.de>
2019-11-07 14:31:32 +0100

    postbuild

---
+++ client/build/precache-manifest.7f661a707d4ed564d2b27b933aed3adc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a52fad48f16f9888e2996af027f04ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4460bad7c09ec624b358",
+    "url": "/static/css/main.b0a860bc.chunk.css"
+  },
+  {
+    "revision": "814b27ec160134161b96",
+    "url": "/static/js/2.2489274c.chunk.js"
+  },
+  {
+    "revision": "4460bad7c09ec624b358",
+    "url": "/static/js/main.375bf10a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
